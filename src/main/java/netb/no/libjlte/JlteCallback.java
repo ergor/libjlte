cd3d2759289b6bbdc88ceacba8e9d8a4f9d77491
Erol Gorancic <erolg@protonmail.com>
2020-04-10 13:13:04 +0200

    added some javadoc to callback methods

--- src/main/java/netb/no/libjlte/JlteCallback.java
+++ src/main/java/netb/no/libjlte/JlteCallback.java
@@ -5,26 +5,37 @@ import javax.swing.text.html.HTML;
 import javax.swing.text.html.HTMLEditorKit;
 
 public class JlteCallback extends HTMLEditorKit.ParserCallback {
+
+    /**
+     * Callback for when text is encountered in between tags.
+     */
     @Override
     public void handleText(char[] chars, int i) {
         super.handleText(chars, i);
     }
 
-    @Override
-    public void handleComment(char[] chars, int i) {
-        super.handleComment(chars, i);
-    }
-
+    /**
+     * Callback for when a tag defined in {@link javax.swing.text.html.parser.DTD} is encountered.
+     */
     @Override
     public void handleStartTag(HTML.Tag tag, MutableAttributeSet mutableAttributeSet, int i) {
         super.handleStartTag(tag, mutableAttributeSet, i);
     }
 
+    /**
+     * Callback for when a tag defined in {@link javax.swing.text.html.parser.DTD} is ended.
+     */
     @Override
     public void handleEndTag(HTML.Tag tag, int i) {
         super.handleEndTag(tag, i);
     }
 
+    /**
+     * "Simple Tags" seems to be any tag not defined in {@link javax.swing.text.html.parser.DTD}.
+     * There is only a limited set defined in that DTD. For instance, the p tag is defined, but div is not.
+     * This method handles both start and end tags for "simple" tags. If it is an end tag, it will be flagged
+     * as such in the {@link MutableAttributeSet} param.
+     */
     @Override
     public void handleSimpleTag(HTML.Tag tag, MutableAttributeSet mutableAttributeSet, int i) {
         super.handleSimpleTag(tag, mutableAttributeSet, i);
